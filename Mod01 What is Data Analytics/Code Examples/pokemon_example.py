19c3a0fb37ebb333ae729847aba2054875e304eb
esanc25 <e.sanc25@gmail.com>
2026-09-16 14:07:04 -0400

    Pushing eveyrhting

--- Mod01 What is Data Analytics/Code Examples/pokemon_example.py
+++
@@ -1,9 +0,0 @@
-# import the thing that lets us use data frames
-
-# use Python's package manager (pip) to install a package that we need
-# pip install <<PACKAGE NAME>>
-
-# pandas
-# pip install pandas
-# "as pd" makes "pd" a nickname for the package
-import pandas as pd
